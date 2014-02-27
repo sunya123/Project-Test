a4fa8134443982f653491902e6414b7b2a7e5b54
sunya123 <sunyanhaozz@163.com>
2014-02-27 15:25:08 +0800

    add if cause

--- ruststudy/hello.rs
+++ ruststudy/hello.rs
@@ -10,5 +10,23 @@ fn main() {
 	println!("see {}",hi);
 
 	println!("hello world");
+
+	static MONSTER_FACTOR: f64 = 57.8;
+	//let monster_size = MONSTER_FACTOR * 10.0;
+	let monster_size: int = 50;
+	println!("the data is {}",MONSTER_FACTOR);
+	println(format!("repeat print the data {}",MONSTER_FACTOR));
+	println(format!("the monster size is {}",monster_size));
+
+	let price;
+	let item="ddsdml";
+	if item == "salad" {
+    	price = 3.50;
+	} else if item == "muffin" {
+    	price = 2.25;
+	} else {
+    	price = 2.00;
+	}
+	println!("the price is {}",price)
 	
 }
