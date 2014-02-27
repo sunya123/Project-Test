44facad095789080a3900bc51e2ec383816a7f7f
sunya123 <sunyanhaozz@163.com>
2014-02-27 14:55:51 +0800

    rust lang study

---
+++ ruststudy/hello.rs
@@ -0,0 +1,14 @@
+fn main() {
+	let hi = "hi";
+	let mut count = 0;
+
+	while count < 10 {
+   		println!("count is {}", count);
+    	count += 1;
+	}
+
+	println!("see {}",hi);
+
+	println!("hello world");
+	
+}
